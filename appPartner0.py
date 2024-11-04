45f3aa77a170b698fd4b7c6ef00134fd9e6ddca7
CharlesLIU <liushaoqiangol@163.com>
2024-11-04 19:08:47 +0800

    commit the appPartner0 for response time test. - 20241104

--- appPartner0.py
+++ appPartner0.py
@@ -16,7 +16,7 @@ from datetime import datetime, timedelta
 
 # Streamed response emulator (generator function)
 def response_generator():
-    response = "对话结束，请返回输入暗号继续作答问卷。暗号：52013141"
+    response = "对话结束，请返回输入暗号继续作答问卷。暗号：52013140"
     for word in response.split():
         yield word + " "  # yield: produce a series of values over time, rather than computing them at once and sending them back like a list.
         time.sleep(0.001)
@@ -47,13 +47,19 @@ if "messages" not in st.session_state:  # added a check to see if the messages k
         {"role": "system", "content": "Your name is Li. You speak Chinese. You are a fictional romantic partner and you are going to plan a travelling plan with me. Your tone should not be that polite or passionate. Your sentences should not exceed three."}
         )
 
-delayTlist = [1, 3]
+delayTlist = [0, 1, 3]
 delayTlist.extend(range(5, 181, 5))
 delayTlist.reverse()  # Reverse the list to avoid performance decrease if using pop(0).
 
+delayT2 = [0, 1, 3, 5]
+
 # Add delayTlist into the session_state
 if "delayTlist" not in st.session_state:
-    st.session_state.delayTlist = delayTlist
+    st.session_state.delayTlist = delayT2
+    
+# Add responseSpeed into the session state
+if "responseSpeeds" not in st.session_state:
+    st.session_state["responseSpeeds"] = []
 
 
 # Display chat messages from history on app rerun (eliminate the system prompt)
@@ -62,7 +68,10 @@ for message in st.session_state.messages[1:]:
         st.markdown(message["content"])
 
 # delayT = random.randint(3, 20)  # randomize deplayed time interval
-delayT = st.session_state["delayTlist"].pop()  # pop up delayT orderly in delayTlist in the dicitionary-like session_state.
+try:
+    delayT = st.session_state["delayTlist"].pop()  # pop up delayT orderly in delayTlist in the dicitionary-like session_state.
+except Exception:
+    pass  # Ignore all exceptions
 
 # Accept user input and React to the user
 if prompt := st.chat_input("在干啥？"):  # We used the := operator to assign the user's input to the prompt variable and checked if it's not None in the same line.
@@ -85,13 +94,16 @@ if prompt := st.chat_input("在干啥？"):  # We used the := operator to assign
         #     status.update(label="已读 :white_check_mark:", state="complete", expanded=False)  # Set the readreceipt
     st.session_state.messages.append({"role": "user", "content": prompt, "avatar": ":material/account_circle:"})  # Add user message to chat history
     
+    try:
+        time.sleep(delayT)  # waiting for response
+    except Exception:
+        pass
     
-    time.sleep(delayT)  # waiting for response
     nowT = datetime.now().strftime("%H:%M:%S")  # Get current time
     st.write(f"<p style='backgroud-color:gray; text-align:center; font-size:10px'> {nowT} </p>", unsafe_allow_html=True)  # Display current time
     
     # Display assistant response in chat message container
-    if len(st.session_state.messages) < 40:  # Allow at most 20-turn conversation
+    if len(st.session_state.messages) <= 6:  # Allow at most 20-turn conversation
         with st.chat_message("assistant", avatar=":material/face:"):
             stream = client.chat.completions.create(
                 model = st.session_state["openai_model"],
@@ -102,10 +114,24 @@ if prompt := st.chat_input("在干啥？"):  # We used the := operator to assign
                 stream = True,
             )
             response = st.write_stream(stream)  # Response
+        
+        # Request participants to rate the response speed.
+        time.sleep(2)
+        col1, col2 = st.columns([1, 1])
+        with col2:
+            st.write("*请对虚拟对象的回复速度打分。*")
+        #     responseSpeed = st.slider(
+        #         label="*你认为虚拟对象的回复在多大程度上是不及时的？（1=非常及时；7=非常不及时）*", 
+        #         min_value=1, 
+        #         max_value=7, 
+        #         value=4)
+        # st.session_state.responseSpeeds.append(responseSpeed)  # Archive the rating marks into the session state
+        
     else:
         with st.chat_message("assistant", avatar=":material/face:"):
             response = st.write_stream(response_generator())  # When exceeding conversation turns
     st.session_state.messages.append({"role": "assistant", "content": response, "avatar": ":material/face:"})
+    
+    
 
-    
-    
+
